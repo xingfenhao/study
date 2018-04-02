c52df7b3edaae6398d46368ddd84f4a731a942d1
xingfenhao <xingfenhao@yingyinglicai.com>
2018-04-02 17:08:32 +0800

    测试对象转换工具类型

---
+++ src/main/java/TestConvert.java
@@ -0,0 +1,12 @@
+/**
+ * yingyinglicai.com Inc.
+ * Copyright (c) 2013-2018 All Rights Reserved.
+ */
+
+/**
+ * 对象转换
+ * @author fenghao.xing
+ * @version : TestConvert.java, v 0.1 2018-04-02 17:07 fenhao.xing Exp $
+ */
+public class TestConvert {
+}
